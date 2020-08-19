f09bfc05b4c666abf51e37219fc8c8a684ec9160
Thiago de Carvalho Corrêa <thiagodecarvalhocorrea@gmail.com>
2020-08-19 16:36:56 -0300

    word-count

---
+++ word-count/word_count.ex
@@ -0,0 +1,17 @@
+defmodule WordCount do
+
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+
+    sentence
+      |> String.downcase()
+      |> String.split(~r/(?![[:alnum:]-])./u, trim: true)
+      |> Enum.frequencies()
+  end
+
+end
