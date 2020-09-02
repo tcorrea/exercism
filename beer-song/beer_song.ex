46697a81cbb177527acc01defa4df84915c18104
Thiago de Carvalho Corrêa <thiagodecarvalhocorrea@gmail.com>
2020-09-02 12:21:43 -0300

    beer-song-solution

---
+++ beer-song/beer_song.ex
@@ -0,0 +1,38 @@
+defmodule BeerSong do
+
+  def verse(0)do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1)do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2)do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t()
+  def verse(number) do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+  end
+
+  def lyrics(range \\ 99..0)do
+    Enum.map_join(range, "\n", fn x -> verse(x) end)
+  end
+end
